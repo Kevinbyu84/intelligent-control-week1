e0cc9cf00b22b2f5064848c5e71cff029228c13f
Kevinbyu84 <kevinmedia84@gmail.com>
2025-02-17 10:46:37 +0700

    menghapus hijau

--- Assignment Deteksi Warna.py
+++ Assignment Deteksi Warna.py
@@ -20,9 +20,6 @@ while True:
     lower_blue = np.array([94, 80, 2])
     upper_blue = np.array([126, 255, 255])
 
-    # Rentang warna hijau dalam HSV
-    lower_green = np.array([25, 52, 72])
-    upper_green = np.array([102, 255, 255])
 
     # Rentang warna kuning dalam HSV
     lower_yellow = np.array([20, 100, 100])
@@ -31,7 +28,6 @@ while True:
     # Masking untuk mendeteksi warna merah, biru, hijau, dan kuning
     mask_red = cv2.inRange(hsv, lower_red, upper_red)
     mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
-    mask_green = cv2.inRange(hsv, lower_green, upper_green)
     mask_yellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
 
     result = frame.copy()
@@ -51,14 +47,6 @@ while True:
         if w * h > 10000:  # Hanya tampilkan bounding box yang besar
             cv2.rectangle(result, (x, y), (x + w, y + h), (255, 0, 0), 2)  # Warna biru
             cv2.putText(result, "Biru", (x + 5, y + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-
-    # Menemukan kontur untuk warna hijau
-    contours_green, _ = cv2.findContours(mask_green, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    for contour in contours_green:
-        x, y, w, h = cv2.boundingRect(contour)
-        if w * h > 10000:  # Hanya tampilkan bounding box yang besar
-            cv2.rectangle(result, (x, y), (x + w, y + h), (0, 255, 0), 2)  # Warna hijau
-            cv2.putText(result, "Hijau", (x + 5, y + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
     # Menemukan kontur untuk warna kuning
     contours_yellow, _ = cv2.findContours(mask_yellow, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
